4f653581601ae9b91064f850f55d9abdc98f2f10
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-03-17 21:22:48 +0530

    01

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,8 @@
+const accountId = 15462
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "123456789"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
