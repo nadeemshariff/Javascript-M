925b729dfae41707150e5a9ccd2235cad3a4dfb2
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-03-18 01:45:49 +0530

    Date and Time

---
+++ 01-basics/07-dates.js
@@ -0,0 +1,33 @@
+//Dates
+
+// let myDate = new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("2024-01-31")
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()  + 1);
+console.log(newDate.getDay());
+
+// String Interpolation - `${newDate.getDay()} and the time`
+newDate.toLocaleString('default', {
+    weekday: 'long',
+    hour: 'numeric',
+    minute: 'numeric'
+
+});
