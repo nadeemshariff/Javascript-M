adb723d3690c29b0e30cbbf7009cb49082d4224a
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-03-17 21:45:11 +0530

    data types

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -2,7 +2,13 @@ const accountId = 15462
 let accountEmail = "xyz@gmail.com"
 var accountPassword = "123456789"
 accountCity = "jaipur"
+let accountState; // gives undefined value
 
-console.log(accountId);
-console.log(accountEmail);
-console.log(accountPassword);
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+
+// Prefer not to use Var because of issue of scope issues
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//alert(3 + 3) //we are using nodejs not browser
+
+console.log(30+3 + "Nadeem");
+
+let name = "Nadeem"
+let age = 24
+let isLoggedIn = false
+let state; //= null
+
+// number => 2 to power 53
+// bigint
+//string => " "
+//boolean => true/false
+//null - a standalone value 
+//undefined - variable that has no value assigned yet
+//symbol -> unique components
+//object => {} or []
+
+console.log(typeof "Nadeem" + name + "is of type "+ typeof name);
